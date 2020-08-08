7a562ca835f8d7fb893bfe36d740e48c22483271
silvanarl <ramossilvana495@gmail.com>
2020-08-07 22:40:44 -0500

    cambio de ext

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
