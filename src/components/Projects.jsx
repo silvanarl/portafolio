7173ba7ec8b21161ab569a1ef704f153a1df77c8
silvanarl <ramossilvana495@gmail.com>
2020-09-10 12:14:03 -0500

    corrigiendo links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,14 +33,14 @@ const Projects = () => (
         <div className="infoProject column">
           <p className="titleProject spanBySkill">POKEDEX</p>
           <a
-            href="https://github.com/silvanarl/LIM012-data-lovers/src/"
+            href="https://silvanarl.github.io/LIM012-data-lovers/src/"
             target="_blank"
             rel="noreferrer"
           >
             <button className="buttonDemo" type="button">Demo</button>
           </a>
           <a
-            href="https://silvanarl.github.io/LIM012-data-lovers/"
+            href="https://github.com/silvanarl/LIM012-data-lovers/"
             target="_blank"
             rel="noreferrer"
           >
@@ -66,14 +66,14 @@ const Projects = () => (
         <div className="infoProject column">
           <p className="titleProject spanBySkill">ROADTIPS</p>
           <a
-            href="https://github.com/silvanarl/LIM012-fe-social-network/src/"
+            href="https://silvanarl.github.io/LIM012-fe-social-network/src/#/"
             target="_blank"
             rel="noreferrer"
           >
             <button className="buttonDemo" type="button">Demo</button>
           </a>
           <a
-            href="https://silvanarl.github.io/LIM012-fe-social-network/"
+            href="https://github.com/silvanarl/LIM012-fe-social-network"
             target="_blank"
             rel="noreferrer"
           >
